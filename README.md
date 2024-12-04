da258b872817cd1c893b110ef047ff8a234e949c
Kevin Sylvestre <kevin@ksylvest.com>
2024-12-03 19:00:20 -0800

    Initialize library

--- README.md
+++ README.md
@@ -1,1 +1,21 @@
 # sourcestack
+
+## Building
+
+```bash
+pip install setuptools
+```
+
+```bash
+bin/build
+```
+
+## Publishing
+
+```bash
+pip install twine
+```
+
+```bash
+bin/publish
+```
---
+++ setup.py
@@ -0,0 +1,24 @@
+from setuptools import setup, find_packages
+
+VERSION = "0.0.1"
+DESCRIPTION = "An API for SourceStack"
+LONG_DESCRIPTION = "A package that makes it easy to use the https://sourcestack.co/"
+
+setup(
+    name="sourcestack",
+    version=VERSION,
+    description=DESCRIPTION,
+    long_description=LONG_DESCRIPTION,
+    author="Kevin Sylvestre",
+    author_email="kevin@ksylvest.com",
+    license="MIT",
+    packages=find_packages(),
+    install_requires=[],
+    keywords="sourcestack",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Intended Audience :: Developers",
+        "License :: OSI Approved :: MIT License",
+        "Programming Language :: Python :: 3",
+    ],
+)
