25e28c12b6f45fabb3063e1d2ce4dfa5df4a2346
Kevin Sylvestre <kevin@ksylvest.com>
2024-12-05 10:21:21 -0800

    Annotate long description content type

--- setup.py
+++ setup.py
@@ -5,12 +5,14 @@ DESCRIPTION = "A python package for using the https://sourcestack.co/ API."
 
 with open("README.md", "r", encoding="utf-8") as file:
     LONG_DESCRIPTION = file.read()
+    LONG_DESCRIPTION_CONTENT_TYPE = "text/markdown"
 
 setup(
     name="sourcestack",
     version=VERSION,
     description=DESCRIPTION,
     long_description=LONG_DESCRIPTION,
+    long_description_content_type=LONG_DESCRIPTION_CONTENT_TYPE,
     author="Kevin Sylvestre",
     author_email="kevin@ksylvest.com",
     license="MIT",
